531112d129c1de7f9c507a29c86aa5f6560a4950
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-11-12 15:54:28 +0200

    the first commit

---
+++ firsthelloworld/Program.cs
@@ -0,0 +1,86 @@
+﻿// 10 + 20
+// x + 10
+
+
+// TYPE NAME = EXPRESSION
+
+int number = 100;
+//int- numere fara virgula -2147....+2147...        
+// byte- 0-255
+//long- 99999999999999999
+//float - numere cu virgula s(-3.1446846098406940)
+// double- numere cu virgula (-3.14789080498704987049780948704987049870498704780478....)
+//char- caractere
+// bool- True(1) or False(0)
+// string- siruri de caractere
+//DateTime- data
+
+string myName = "Emi";
+float number2 = 100.0f;
+char character = 'a';
+
+Console.WriteLine(number);
+
+// char- int- long- float -double
+// double- float-long- int- char---problema
+
+int myNumber = 100;
+long myNumberLong = myNumber;
+
+// Console.ReadLine(rezult) citire de la utilizator
+
+
+//string name = Console.ReadLine();
+
+
+//Console.WriteLine("care este numele tau?");
+
+//string name = Console.ReadLine("Salut " + name);
+
+
+//OPERATORI
+//+ adunare
+// - scadere
+// * inmultire
+// / impartire
+// ++ increment- cresc cu unu
+//__decrement- scad cu 1
+// % modulo- ne returneaza rezultatul impartirii nr la ce ii dam noi
+
+
+int result = 10 % 3; // restul e 1
+
+
+// ASSIGNMENT
+// Comparare
+
+// == equal to- doar valoare boolian
+Console.WriteLine(5 == 5);
+Console.WriteLine(5 == 6);
+
+// != not equal
+// greater than >
+// less than <
+// >=
+//<=
+
+
+
+//operatori logici...if
+
+// &&- logical and
+// || logical or
+// ! logical not
+
+//Console.WriteLine(6<5 && 3<2);     //
+
+
+//Conditia if
+
+
+switch(DayOfWeek)
+    break
+
+
+
+
