641e89aaf1b511be18249b2a8bb3282980049eb2
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-11-16 22:39:22 +0200

    Tema 3 Exercitiul 8

---
+++ T3 e 8/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int temp;
+temp = Convert.ToInt32(Console.ReadLine());
+if (temp < 0)
+    Console.Write("Freezing weather");
+else if (temp < 10)
+    Console.Write("Very cold weather");
+else if (temp < 20)
+    Console.Write("Cold weather");
+else if (temp < 30)
+    Console.Write("Normal in temp");
+else if (temp < 40)
+    Console.Write("Its Hot");
+else
+    Console.Write("Its very hot");
+
