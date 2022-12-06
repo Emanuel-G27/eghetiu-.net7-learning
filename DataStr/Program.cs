3fbcd407f849580243da5367410ddd2dbdb6b8b4
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-12-06 23:35:20 +0200

    Data Structure Ex 2

---
+++ DataStr/Program.cs
@@ -0,0 +1,14 @@
+﻿class DataStructure
+{
+    public static void Main(string[] args)
+    {
+        int[,] array = { { 123, 345, 567},
+                         {12, 56, -12 },
+                         {34, -98, 65 },
+                         {765, 543,321 }
+                       };
+
+        Console.WriteLine(array[1, 1]);
+        Console.WriteLine(array[2, 1]);
+    }
+}
