ffb52f28b0538e1ef3874ac25cbcb0da569496cf
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-12-06 23:21:43 +0200

    DataS Exercitiul 5

---
+++ Recapitulare1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+
+class Recapitulare
+{
+    public static void Main(string[] args)
+    {
+      //  Using a queue which has 5 elements
+      // (read from the user and added to the
+      // queue one by one) check if the top of 
+      //the queue is a positive number and a multiple
+      // of 3 or a multiple of 7.If not remove the top
+      // of the queue and print the new top of the queue.
+
+        // nu am inteles asa bine cerinta, nu stiam daca trebuie indeplinite toate cele 3 cerinte de pozitiv,multiplu de 3 si 7
+        
+
+       Queue<int> nr = new Queue<int>();
+        nr.Enqueue(-17);
+        nr.Enqueue(3);
+        nr.Enqueue(45);          
+        nr.Enqueue(12);
+        nr.Enqueue(5);
+
+        int x = nr.Dequeue();
+        
+        if (x < 0)
+        {
+            Console.WriteLine("The top of the queue({0}) is negative", x);
+            Console.WriteLine("The new top element of my queue is: {0}", nr.Peek());
+        }
+       
+                                               
+        
+
+
+
+    }
+
+
+
+}
