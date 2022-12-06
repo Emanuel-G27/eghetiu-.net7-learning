f5a5bd92f1630be3cf25b4ddfbb363c15b4cee20
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-12-06 23:48:11 +0200

    Data Structure ex 4

---
+++ DataStructures4/Program.cs
@@ -0,0 +1,19 @@
+﻿class DataStructures
+{
+    public static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        stack.Push(30);
+        stack.Push(56);
+
+        string result1;
+        string result;
+        int poz = stack.ElementAt(1);
+        int poz1 = stack.ElementAt(0);
+
+        result = poz + poz1 == 30 ? "true" : "false";
+        Console.WriteLine(result);
+        result1 = poz == 30 ? "true" : "false";
+        Console.WriteLine(result1);
+    }
+}
