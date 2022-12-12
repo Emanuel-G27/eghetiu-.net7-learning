348e0ebc6948db0a5f66016d98404b1ad939469f
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-12-12 20:58:27 +0200

    Week1 OOP Ex2

---
+++ OOP.Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+using OOP.Ex2;
+
+Product product = new Product("Apple", 0.99f, 10);
+
+Console.WriteLine(product.GetName()); 
+Console.WriteLine(product.GetPrice()); 
+Console.WriteLine(product.GetQuantity()); 
+
+product.SetName("Orange");
+product.SetPrice(1.49f);
+product.SetQuantity(20);
+
+Console.WriteLine();
+
+Console.WriteLine(product.GetName()); 
+Console.WriteLine(product.GetPrice()); 
+Console.WriteLine(product.GetQuantity()); 
