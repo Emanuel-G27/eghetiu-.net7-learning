e4283f0a8ec73f8a7e41339f1f61cb42ddfbce61
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2022-12-19 23:35:24 +0200

    Week1 OOP Ex5

---
+++ OOP.Ex5/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Ex5
+{
+    public class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public double StudentId;
+        public double GPA;
+        
+
+        public Student(string FirstName, string LastName, double StudentId, double GPA)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StudentId = StudentId;
+            this. GPA = GPA;
+        }
+
+        public string GetFullName(string FirstName, string LastName)
+        {
+            return FirstName + " " + LastName;
+        }
+        public bool HasHonors()
+        {
+            if (GPA >= 3.5)
+                return true;
+            else
+                return false;
+                                                              
+        }
+
+        
+                                 
+                    
+        
+
+      
+            
+        
+    }
+}
