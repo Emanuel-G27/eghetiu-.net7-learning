7fc8719d7deaf860d5d21d3c8d4ac793bc00ad58
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2023-01-02 15:40:32 +0200

    OOP.Advanced.Ex2

---
+++ Teme.OOP.W2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+using System;
+using Teme.OOP.W2;
+
+Book book = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+
+Console.WriteLine(book.GetTitle()); 
+Console.WriteLine(book.GetAuthor()); 
+Console.WriteLine(book.GetPageCount()); 
+Console.WriteLine(book.GetCurrentPage());
+
+book.NextPage();
+Console.WriteLine(book.GetCurrentPage()); 
+
+book.NextPage();
+book.NextPage();
+book.NextPage();
+Console.WriteLine(book.GetCurrentPage()); 
+
+book.PreviousPage();
+Console.WriteLine(book.GetCurrentPage()); 
+
+book.Title = "The Great Gatsby (Revised Edition)";
+Console.WriteLine(book.GetTitle()); 
+
+book.Author = "F. Scott Fitzgerald (Revised by Jane Doe)";
+Console.WriteLine(book.GetAuthor()); 
