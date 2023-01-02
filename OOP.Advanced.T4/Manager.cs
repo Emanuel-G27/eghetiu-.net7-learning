9d1a2b43ff6c76583b8773d2ee40c386399efb98
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2023-01-02 22:18:30 +0200

    OOP.Advanced.Ex. 4

---
+++ OOP.Advanced.T4/Manager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Advanced.T4
+{
+    public class Manager: Employee
+    {
+        private Department iT;
+        public Department IT { get; set; }
+
+        public Manager(string name, int age, double salary, Department IT)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Salary = salary;
+            this.iT = IT;
+        }
+
+        public override double CalculateBonus()
+        {
+            double bonus;
+            return bonus = Salary * 0.1;
+        }
+        public override string GetContactInfo()
+        {
+            return $"Name: {Name}, Age: {Age}, Department:{Department.IT}, Email: {Email}, Phone: {Phone}";     
+        }
+
+    }
+}
---
+++ OOP.Advanced.T4/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using OOP.Advanced.T4;
+using System;
+
+Manager manager = new Manager("John Doe", 35, 50000, Department.IT);
+Developer developer = new Developer("Jane Smith", 28, 30000, new List<string> { "C#", "JavaScript" }, false);
+
+Console.WriteLine(manager.CalculateBonus()); 
+Console.WriteLine(developer.CalculateBonus()); 
+
+Console.WriteLine(manager.GetContactInfo());
