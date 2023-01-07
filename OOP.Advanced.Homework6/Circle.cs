bceeefd416a568718150f435fc9f8eea62033eb5
Emanuel-G27 <117767297+Emanuel-G27@users.noreply.github.com>
2023-01-07 20:09:04 +0200

    OOP.Homework.Ex6

---
+++ OOP.Advanced.Homework6/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Advanced.Homework6
+{
+    public class Circle : IShape, IColor
+    {      
+        public double Area { get; set; }
+        private double radius;
+        public double CalculateArea()
+        {            
+            return this.Area = Math.PI * Math.Pow(radius, 2);
+        }
+        public string Color { get; set; }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            this.Color = "Black";
+        }
+        public Circle(double radius, string color)
+        {
+            this.Color = color;
+            this.radius = radius;
+        } 
+        
+        public string CalculateArea(string unitOfMeasurement)
+        {
+            return $"Area: {Area} {unitOfMeasurement}";
+        }
+    
+
+    }
+}
---
+++ OOP.Advanced.Homework6/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Advanced.Homework6
+{
+    public class Rectangle : IShape, IColor
+    {
+        private double width;
+        private double height;       
+        public double Area { get; set; }
+
+        public string Color { get; set; }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+            this.Color = "Black";
+        }
+        public Rectangle(double width, double height, string color)
+        {
+            this.width = width;
+            this.height = height;
+            this.Color = color;
+
+        }
+        public double CalculateArea()
+        {
+            return this.Area = width * height;
+        }
+        public string CalculateArea(string unitOfMeasurement)
+        {
+            return $"Area: {Area} {unitOfMeasurement}";
+        }
+           
+           
+    }
+}
